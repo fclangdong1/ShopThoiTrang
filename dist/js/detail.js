464e0fddfac2572c1dccbdd6765d7408d4a3da17
phanvanha <phanvanha1234567@gmail.com>
2021-07-21 17:18:43 +0700

    commit

--- dist/js/detail.js
+++ dist/js/detail.js
@@ -1,22 +1,20 @@
-$(document).ready(function () {
-    $(".navbar-toggler").click(function () {
-        $('.mainmenu').css('display', 'block');
-    });
-});
-let tabLinks = document.querySelectorAll(".nav-item");
-let tabContent = document.querySelectorAll(".item-container");
-tabLinks.forEach(function (el) {
+var tabLink = document.querySelectorAll(".chitiet");
+var tabContents = document.querySelectorAll(".tab-pane");
+
+tabLink.forEach(function (el) {
     el.addEventListener("click", openTabs);
 });
+
+
 function openTabs(el) {
     var btn = el.currentTarget; // lắng nghe sự kiện và hiển thị các element
     var electronic = btn.dataset.electronic; // lấy giá trị trong data-electronic
 
-    tabContent.forEach(function (el) {
+    tabContents.forEach(function (el) {
         el.classList.remove("active");
     }); //lặp qua các tab content để remove class active
 
-    tabLinks.forEach(function (el) {
+    tabLink.forEach(function (el) {
         el.classList.remove("active");
     }); //lặp qua các tab links để remove class active
 
